e5543be6d4ea73e2e3c1b4ae2d4ced316415345f
Alex Shinn <alexshinn@gmail.com>
2010-07-05 12:48:35 +0000

    incorporating patches from issues #1 and #4 adding documentation for names/submatches allowing named submatches to stack also updating build for chicken 4.0 modules

--- test-irregex.scm
+++ test-irregex.scm
@@ -283,5 +283,24 @@
        (lambda (src i s) (reverse s))))
   )
 
+
+(define (extract name irx str)
+  (irregex-match-substring (irregex-match irx str) name))
+
+(test-group "named submatches"
+  (test "matching alternative is used"
+        "first" (extract 'sub `(or (submatch-named sub "first")
+                                   (submatch-named sub "second"))
+                         "first"))
+  (test "matching alternative is used (second match)"
+        "second" (extract 'sub `(or (submatch-named sub "first")
+                                    (submatch-named sub "second"))
+                         "second"))
+  (test "last match is used with multiple matches for a name"
+        "second" (extract 'sub `(seq (submatch-named sub "first")
+                                     space
+                                     (submatch-named sub "second"))
+                         "first second")))
+
 (test-end)
 
